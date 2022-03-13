7fce3c86c002bc529b4af994dfb89d049e8f5cdc
Junyi Yao <j.yao@wustl.edu>
2022-03-13 17:36:04 -0500

    okay

---
+++ CreationViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CreationViewController.swift
+//  Flashcard
+//
+//  Created by 姚君仪 on 3/13/22.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    var flashcardController: ViewController!
+    
+    @IBOutlet weak var Question: UITextField!
+    @IBOutlet weak var Answer: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        
+        let questionText = Question.text
+        
+        let answerText = Answer.text
+        
+        flashcardController.updateFlashcard(question: questionText!, answer: answerText!)
+        
+        dismiss(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flashcard/ViewController.swift
+++ Flashcard/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BackLabel: UILabel!
+    @IBOutlet weak var FrontLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if(FrontLabel.isHidden == false){
+            FrontLabel.isHidden = true
+    }
+        else{
+            FrontLabel.isHidden = false
+        }
+    
+}
+    func updateFlashcard(question: String, answer: String) {
+        BackLabel.text = answer
+        FrontLabel.text = question
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardController = self
+        
+}
 }
 
