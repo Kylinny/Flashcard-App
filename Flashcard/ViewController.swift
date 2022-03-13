0529dd5a3228042d31bee31a5e094af4d157a0f6
Junyi Yao <j.yao@wustl.edu>
2022-03-13 16:55:17 -0500

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by 姚君仪 on 3/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
